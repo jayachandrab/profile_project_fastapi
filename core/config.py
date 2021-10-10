c684ae1454a37b5e7285bf34ecff49672483ea3b
unknown <b.jayachandrareddy@gmail.com>
2021-10-10 22:36:41 +0530

    commit to database changes

--- core/config.py
+++ core/config.py
@@ -1,4 +1,18 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+env_path=Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+print(env_path)
+print(os.getenv("POSTGRES_USER"))
 class Settings:
     PROJRECT_TITLE:str="Jobboard"
     PROJECT_VERSION:str = "0.1.1"
+    POSTGRES_USER=os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER=os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT=os.getenv("POSTGRES_PORT")
+    POSTGRES_DB=os.getenv("POSTGRES_DB")
+    DATABASE_URL=f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 settings=Settings()
